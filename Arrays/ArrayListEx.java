3ba928c038548e3a33003d3596b441c6432040ac
gagan13singh <gagandeepsingh130305@gmail.com>
2026-01-27 14:33:37 +0530

    arrayList and 2d arrays

---
+++ Arrays/ArrayListEx.java
@@ -0,0 +1,15 @@
+package Arrays;
+//similar to vectors in cpp
+
+import java.util.ArrayList;
+
+public class ArrayListEx {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(10);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
+    }
+}
