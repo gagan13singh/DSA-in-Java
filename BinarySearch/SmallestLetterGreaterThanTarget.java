4140952a4af61aa0e33e15237b2e1e1af334cb74
gagan13singh <gagandeepsingh130305@gmail.com>
2026-02-05 14:38:09 +0530

    SmallestLetterGreaterThanTarget

---
+++ BinarySearch/SmallestLetterGreaterThanTarget.java
@@ -0,0 +1,25 @@
+package BinarySearch;
+
+public class SmallestLetterGreaterThanTarget {
+    public static void main(String[] args) {
+        char[] arr = {'c', 'f', 'j'};
+        char key = 'a';
+        System.out.println(nextGreatestLetter(arr, key));
+    }
+
+    static char nextGreatestLetter(char[] arr, char target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return arr[start % arr.length];
+    }
+}
