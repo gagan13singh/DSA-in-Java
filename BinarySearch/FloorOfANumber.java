b3a47c9441a00c653992eaf02ea29ae678136b63
gagan13singh <gagandeepsingh130305@gmail.com>
2026-02-05 13:57:25 +0530

    ceiling and floor of a number

--- BinarySearch/FloorOfANumber.java
+++ BinarySearch/FloorOfANumber.java
@@ -1,21 +1,18 @@
 package BinarySearch;
 
-public class CeilingOfANumber {
+public class FloorOfANumber {
     public static void main(String[] args) {
-        int[] arr = {-12, 0, 1, 3, 45, 65, 76};
-        int key = 55;
-        CeilingValue(arr, key);
-
-
+        int[] arr={-12,0,1,3,45,65,76};
+        int key=55;
+        FloorVal(arr,key);
     }
-
-    static void CeilingValue(int[] arr, int key) {
+    static void FloorVal(int[] arr, int key) {
         int start = 0;
         int end = arr.length - 1;
-        if (key > arr[arr.length - 1]) {
-            System.out.println("ceiling doesn't exist");
-            }
-        else{
+
+        if (key < arr[0])
+            System.out.println("floor dosen't exist");
+        else {
             while (start <= end) {
                 int mid = start + (end - start) / 2;
                 if (key < arr[mid]) {
@@ -27,7 +24,7 @@ public class CeilingOfANumber {
                     return;
                 }
             }
-            System.out.println(arr[start]);
+            System.out.println(arr[end]);
         }
     }
 }
