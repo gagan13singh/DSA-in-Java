632e92c0bd5060ce1cff29f2b08c7f92574ef9aa
gagan13singh <gagandeepsingh130305@gmail.com>
2026-02-09 13:40:09 +0530

    Counting how many times an array is rotated

---
+++ BinarySearch/RotationCount.java
@@ -0,0 +1,28 @@
+package BinarySearch;
+
+public class RotationCount {
+    public static void main(String[] args) {
+        int[] arr={4,5,6,7,8,1,2,3};
+        System.out.println(findPivot(arr)+1);
+    }
+    static int findPivot(int[]arr){
+        int start=0,end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(mid<end && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            if(mid>start && arr[mid]<arr[mid-1]){
+                return mid-1;
+            }
+            if(arr[mid]<=arr[start]){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+
+        }
+        return -1;
+    }
+}
