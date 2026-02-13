7b63c156fc7a33821fb06ad7be3cc5a623477e71
gagan13singh <gagandeepsingh130305@gmail.com>
2026-02-13 14:31:54 +0530

    cyclic sort find duplicates

---
+++ Sorting/FindDuplicates.java
@@ -0,0 +1,35 @@
+package Sorting;
+
+//leetcode version
+
+class FindDuplicates {
+
+    public int findDuplicate(int[] arr) {
+        int i = 0;
+
+        while (i < arr.length) {
+
+            if (arr[i] != i + 1) {
+
+                int correctIndex = arr[i] - 1;
+
+                if (arr[i] != arr[correctIndex]) {
+                    swap(arr, i, correctIndex);
+                } else {
+                    return arr[i]; // duplicate found
+                }
+
+            } else {
+                i++;
+            }
+        }
+
+        return -1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
