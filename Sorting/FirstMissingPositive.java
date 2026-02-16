d8c9f9e6e98dc0707aab2df524bc07c44ec174a7
gagan13singh <gagandeepsingh130305@gmail.com>
2026-02-16 13:21:44 +0530

    cyclic sort first missing positive

---
+++ Sorting/FirstMissingPositive.java
@@ -0,0 +1,32 @@
+package Sorting;
+
+public class FirstMissingPositive {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,3,4,8,9,6};
+        System.out.println(missingNumber(arr));
+    }
+
+
+    static int missingNumber ( int[] arr){
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i]>0 && arr[i] <= arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index+1)
+                return index+1;
+        }
+        return arr.length+1;
+    }
+
+    static void swap ( int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
