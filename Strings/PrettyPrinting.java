07dea33a4bd2399ad1cefae8595804d8cd69fdc5
gagan13singh <gagandeepsingh130305@gmail.com>
2026-02-18 11:43:17 +0530

    Pretty Printing in java

---
+++ Strings/PrettyPrinting.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a=453.45345f;
+//        System.out.printf("Formatted number is %.2f" , a);
+//        System.out.printf("%.3f", Math.PI);
+        System.out.printf("hello, my name is %s","gagan");
+    }
+}
+// for more placeholders open placeholder_list.txt
