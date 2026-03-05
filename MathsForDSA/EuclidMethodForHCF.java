3c8ff0b6bd325b7722ecc4935ebb2d74c4ddb83c
gagan13singh <gagandeepsingh130305@gmail.com>
2026-03-05 14:31:04 +0530

    GCD & LCM

---
+++ MathsForDSA/EuclidMethodForHCF.java
@@ -0,0 +1,18 @@
+package MathsForDSA;
+
+public class EuclidMethodForHCF {
+    public static void main(String[] args) {
+        System.out.println(gcd(20,30));
+        System.out.println(lcm(20,30));
+    }
+
+    static int gcd(int a,int b){
+        if(a==0){
+            return b;
+        }
+        return gcd(b%a,a);
+    }
+    static int lcm(int a,int b){
+        return a*b/gcd(a,b);
+    }
+}
