f43e5c71a8775f6c97454cfd5f3b612f483ce532
gagan13singh <gagandeepsingh130305@gmail.com>
2026-03-05 12:55:03 +0530

    Flipping image leetcode ques

---
+++ MathsForDSA/FlipplingImage.java
@@ -0,0 +1,16 @@
+package MathsForDSA;
+
+public class FlipplingImage {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for(int[] row:image){
+            //reversing an array
+           for(int i=0;i<(image.length+1)/2;i++){
+               int temp=row[i]^1;
+               row[i]=row[image[0].length-1-i]^1;
+               row[image[0].length-1-i]=temp;
+
+           }
+        }
+        return image;
+    }
+}
