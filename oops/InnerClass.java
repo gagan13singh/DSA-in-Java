af491f42f1b7b6f27b5dc269e3f5e248e302b652
gagan13singh <gagandeepsingh130305@gmail.com>
2026-06-20 19:03:10 +0530

    inner classes

---
+++ oops/InnerClass.java
@@ -0,0 +1,15 @@
+package oops;
+
+public class InnerClass {
+    static class Test{
+
+    }
+}
+
+//Outside classes cant be static.
+//Inner classes can or cannot be static.
+//An inner class is a class defined inside another class.
+// It helps logically group related classes and allows the inner class
+// to access all members of the outer class, including private members.
+// A static inner class can be created without creating an object
+// of the outer class.
