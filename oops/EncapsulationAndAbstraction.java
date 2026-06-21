5a79d0c9a2c60503d001d672397b61fcb3bbae59
gagan13singh <gagandeepsingh130305@gmail.com>
2026-06-21 14:58:21 +0530

    Encapsulation and Abstraction difference

---
+++ oops/EncapsulationAndAbstraction.java
@@ -0,0 +1,16 @@
+package oops;
+
+public class EncapsulationAndAbstraction {
+}
+
+//Encapsulation:-
+//Wrapping data (variables) and methods (functions) into a single unit and restricting direct access to the data.
+//Usually achieved using:
+//private variables
+//Public getters and setters
+
+//Abstraction:
+//Hiding implementation details and showing only essential functionality.
+
+//Encapsulation = Data Hiding
+//Abstraction   = Implementation Hiding
