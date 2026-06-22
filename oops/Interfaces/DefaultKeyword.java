073cee3e3f53d0c2a8ef319f960ada5d42508ee2
gagan13singh <gagandeepsingh130305@gmail.com>
2026-06-22 19:44:34 +0530

    default keyword in interfaces

---
+++ oops/Interfaces/DefaultKeyword.java
@@ -0,0 +1,23 @@
+package oops.Interfaces;
+
+//public class DefaultKeyword implements A,B {
+//}
+//interface A {
+//    default void show() {
+//        System.out.println("A");
+//    }
+//}
+//
+//interface B {
+//    default void show() {
+//        System.out.println("B");
+//    }
+//}
+
+// Default methods:
+// 1. Introduced in Java 8.
+// 2. Have a body inside an interface.
+// 3. Implementing classes may use them directly.
+// 4. Can be overridden.
+// 5. Help add new methods to interfaces without breaking existing code.
+
