79b35f5a58cd3a5cced418e6c034f3a7e9491f38
gagan13singh <gagandeepsingh130305@gmail.com>
2026-06-22 19:52:33 +0530

    Nested interfaces

---
+++ oops/Interfaces/NestedTest.java
@@ -0,0 +1,24 @@
+package oops.Interfaces;
+
+//Nested Interfaces
+
+class A {
+
+    interface B {
+        void greet();
+    }
+}
+ public class NestedTest implements A.B {
+
+    @Override
+    public void greet() {
+        System.out.println("Hello");
+    }
+     public static void main(String[] args) {
+         NestedTest t = new NestedTest();
+         t.greet();
+     }
+
+ }
+
+
